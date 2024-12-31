bb3e1b71de3b2056fb535ea7b9707bf4c96be211
peoplenamed <peoplenamed@gmail.com>
2024-12-31 05:46:33 +0000

    idk

--- flake.nix
+++ flake.nix
@@ -1,40 +1,34 @@
 {
-
   description = "@builder.io/ai-shell";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    snow-blower.url = "github:use-the-fork/snow-blower";
+
+    # Use an absolute or relative path (no '~') for local flakes
+    #snow-blower.url = "path:/home/hobbes/Documents/snow-blower";
+    #snow-blower.url = "path:/home/hobbes/Documents/snow-blower";
+
+    snow-blower.url = "github:peoplenamed/snow-blower";
+
     dream2nix.url = "github:nix-community/dream2nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs @ {
-    flake-parts,
-    dream2nix,
-    nixpkgs,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs = inputs @ { flake-parts, dream2nix, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      # Optional: Additional modules from `snow-blower`
       imports = [
         inputs.snow-blower.flakeModule
       ];
+
+      # The set of systems this flake will handle
       systems = nixpkgs.lib.systems.flakeExposed;
 
-      perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
-        system,
-        lib,
-        ...
-      }: {
-
-      packages = {
-        # To build the package with nix use ` nix build .#ai-shell`
-        # this will place the resulting package in the /results folder.
-        #
-        # This also allows us to use ai-shell as an input src and install on other systems
-        ai-shell = dream2nix.lib.evalModules {
+      # 1) Define your system-specific configuration
+      perSystem = { pkgs, system, ... }:
+      let
+        # Evaluate the ai-shell using dream2nix
+        aiShell = dream2nix.lib.evalModules {
           packageSets.nixpkgs = dream2nix.inputs.nixpkgs.legacyPackages.${system};
           modules = [
             ./default.nix
@@ -45,73 +39,28 @@
             }
           ];
         };
-        default = self'.packages.ai-shell;
-      };
-        snow-blower = {
-          languages = {
-            #set up JS and node 18 as required by nvmrc
-            javascript.enable = true;
-            javascript.npm = {
-              enable = true;
-              package = pkgs.nodejs_18;
-            };
-          };
+      in
+      {
+        packages = {
+          # The main AI Shell package for this system
+          ai-shell = aiShell;
 
-         integrations = {
-            # tree formater
-            treefmt = {
-              programs = {
-                #formater for nix
-                alejandra.enable = true;
-                #as suggested by docs
-                prettier.enable = true;
-              };
-            };
-
-            # pre-commit hooks
-            git-hooks.hooks = {
-              treefmt.enable = true;
-              eslint.enable = true;
-            };
-
-         };
-
-         # Just runner scripts to mirror package.json
-         scripts = {
-           "start" = {
-             just.enable = true;
-             description = "starts the dev envirment.";
-             exec = ''
-               npm run start
-             '';
-           };
-           "lint" = {
-             just.enable = true;
-             description = "runs prettier and eslint.";
-             exec = ''
-               npm run lint
-             '';
-           };
-           "lint-fix" = {
-             just.enable = true;
-             description = "runs prettier and eslint with `--fix`.";
-             exec = ''
-               npm run lint:fix
-             '';
-           };
-         };
-
-         # Auto runs NPM install if 'node_modules' dosent
-         # exsist when entering the shell.
-         shell.startup = [
-           ''
-             if [[ ! -d node_modules ]]; then
-                 npm install
-             fi
-           ''
-         ];
-
+          # Optional: define a `default` alias to ai-shell
+          # so you can build with `nix build .#packages.x86_64-linux.default`
+          default = aiShell;
         };
+
+        # If you want to define Home Manager configurations here, you can:
+        # homeConfigurations = {
+        #   hobbes = home-manager.lib.homeManagerConfiguration {
+        #     pkgs = pkgs;
+        #     modules = [
+        #       {
+        #         home.packages = [ aiShell ];
+        #       }
+        #     ];
+        #   };
+        # };
       };
     };
 }
